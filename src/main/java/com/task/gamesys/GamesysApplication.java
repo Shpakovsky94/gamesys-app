952ea5270f21e234f6ec89ce72d6c09b2cdf6765
shpakovskyii <oleksandr.shpakovskyi@finnplay.com>
2021-03-17 14:23:57 +0200

    pushed empty project to GIT

---
+++ src/main/java/com/task/gamesys/GamesysApplication.java
@@ -0,0 +1,13 @@
+package com.task.gamesys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamesysApplication.class, args);
+    }
+
+}
