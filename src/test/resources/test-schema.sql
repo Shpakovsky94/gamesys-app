ab6a80ed8f9460571f13aaafc03f98113fe9c550
Oleks Oleks <shpakovsky94@gmail.com>
2021-03-31 22:09:38 +0300

    refactoring after Gamesys review

---
+++ src/test/resources/test-schema.sql
@@ -0,0 +1,30 @@
+drop
+database if exists tweets;
+create
+database tweets;
+use
+tweets;
+
+create table if not exists `tweets`.`ELONS_TWEETS`
+(
+    PK_ROW_ID
+    bigint
+    primary
+    key
+    not
+    null
+    AUTO_INCREMENT,
+    tweet_Id
+    bigint
+    unique
+    not
+    null,
+    created_at
+    varchar
+(
+    100
+) null,
+    text varchar
+(
+    1000
+) null);
